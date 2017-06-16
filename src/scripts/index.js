8fc7131eb4eb41b5065c738f2c3b4915db8868c0
Aspirationtocode <aspiretocode@gmail.com>
2017-06-17 00:55:32 +0300

    implement header

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,27 @@
 import '../views/index.pug';
 import '../styles/main.styl';
+import $ from 'jquery';
 import initHeaderNav from './modules/init-header-nav';
 
 initHeaderNav();
+
+const beforeCar = $('.header-cars__element--before');
+const afterCar = $('.header-cars__element--after');
+const headerLocation = $('.header-location');
+
+animate(beforeCar, 1000);
+animate(headerLocation, 1000);
+animate(afterCar, 2000);
+
+function animate(element, timeout) {
+  if (timeout) {
+    setTimeout(
+      () => {
+        element.addClass('animated');
+      },
+      timeout,
+    );
+  } else {
+    element.addClass('animated');
+  }
+}
